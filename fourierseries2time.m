78493ba1201f2f6ce4bea6b6a244e074930fa170
Daniel Tavares <daniel.tavares@lnls.br>
2016-01-13 19:10:32 -0200

    fourierseries2time.m: make function matched with fourieseries

--- fourierseries2time.m
+++ fourierseries2time.m
@@ -1,20 +1,21 @@
-function [y,t] = fourierseries2time(f, coeff, tfinal)
+function [y,t] = fourierseries2time(amp, ph, Fs, npts)
 
-coeff = coeff(:);
+if nargin < 4
+    npts = 2*length(f);
+end
 
-ffty = 2*coeff*length(coeff);
-ffty(1) = coeff(1);
-ffty = [ffty; zeros(length(ffty),1)];
+Y = amp.*exp(1j*ph);
+
+Y = Y(:);
+
+ffty = Y*npts/2;
+ffty(1) = 2*ffty(1);
+
+if npts == 2*(length(Y)-1)
+    ffty(end) = 2*ffty(end);
+end
+    
+ffty = [ffty; zeros(npts-length(Y),1)];
 y = ifft(ffty, 'symmetric');
 
-npts = length(y);
-
-Fs = 2*f(end)/npts*(npts+1);
-
 t = (0:npts-1)/Fs;
-
-if nargin > 2
-    tindex = find(t <= tfinal);
-    t = t(tindex);
-    y = y(tindex);    
-end
